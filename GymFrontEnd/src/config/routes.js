fa1e5d2a36440ecddfde3ec5cb9ca29fc4e92c6b
harshit77 <hsahu77@gmail.com>
2019-09-11 12:20:20 +0530

    Update routes.js

--- GymFrontEnd/src/config/routes.js
+++ GymFrontEnd/src/config/routes.js
@@ -18,7 +18,7 @@ import SignUpWithData from '../components/SignUp.jsx';
  import UpdateUserWithData from '../components/UpdateUser.jsx';
 
 const httplink=createHttpLink({
-  uri:'https://modest-yalow-7daa94.netlify.com/index'
+  uri:'https://modest-yalow-7daa94.netlify.com/.netlify/functions/index'
 });
 
 const authLink=setContext((_,{headers})=>{
